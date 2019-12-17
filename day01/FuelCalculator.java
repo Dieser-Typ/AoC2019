ab7a5f97f25d18d13b5d71b8fc39cd0c9e401844
DieserTyp <lukas.hildebrand@stud.tu-darmstadt.de>
2019-12-17 23:25:49 +0100

    day 1

---
+++ day01/FuelCalculator.java
@@ -0,0 +1,37 @@
+package day01;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FuelCalculator {
+    private static int solution(int part) {
+        try {
+            return new BufferedReader(new FileReader("inputs/day01.txt"))
+                    .lines()
+                    .mapToInt(Integer::parseInt)
+                    .map(part == 1?
+                            FuelCalculator::calcFuel :
+                            FuelCalculator::calcAdditionalFuel)
+                    .reduce(0, Integer::sum);
+        } catch (FileNotFoundException e) {
+            return "I don't give a fuck".length();
+        }
+    }
+
+    private static int calcFuel(int mass) {
+        return mass / 3 - 2;
+    }
+
+    private static int calcAdditionalFuel(int mass) {
+        int fuel = 0;
+        while ((mass = calcFuel(mass)) > 0)
+            fuel += mass;
+        return fuel;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Solution of day 1, part 1: " + solution(1));
+        System.out.println("Solution of day 1, part 2: " + solution(2));
+    }
+}
